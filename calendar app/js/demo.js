9914f9108c98cb80476b1f69c9d6c2ac54acabf1
yuryk200 <82842394+yuryk200@users.noreply.github.com>
2022-04-28 20:25:45 +0100

    Add files via upload

--- calendar app/js/demo.js
+++ calendar app/js/demo.js
@@ -42,11 +42,186 @@ $(document).ready(function() {
   
     
 });
+		
+		
+		function makeList() 
+		{
+			var listData = JSON.parse(localStorage.getItem('events'));
+
+			var listElement = document.getElementById("myList");
+			var listElement2 = document.getElementById("myList2");
+			
+			//var listElement = document.getElementById("html-data-table");
+			//var listElement2 = document.getElementById("html-data-table2");
+   
+			var numberOfListItems = listData.length;
+			var listItem;
+			var i;
+			var newRow = 'name:';
+			var space;
+			var count = 0;
+			var count2 = 0;
+			var count3 = 0;
+			var count4 = 0;
+			var count5 = 0;
+			var count6 = 0;
+
+			for (i = 0; i < numberOfListItems; ++i) 
+			{
+				
+				listItem = document.createElement('li');
+				listItem.innerHTML = listData[i].id;
+				if(count == 1)
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Event ID:';
+					listElement2.appendChild(newRow)
+					listElement2.appendChild(listItem);
+					space = document.createElement('br');
+					listElement2.appendChild(space);
+					count = count - 1;
+				}
+				else
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Event ID:';
+					listElement.appendChild(newRow)
+					listElement.appendChild(listItem);
+					space = document.createElement('br');
+					listElement.appendChild(space);
+					count = count + 1;
+				}
+				
+				listItem = document.createElement('li');
+				listItem.innerHTML = listData[i].name;
+				if(count2 == 1)
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Event Name:';
+					listElement2.appendChild(newRow)
+					listElement2.appendChild(listItem);
+					space = document.createElement('br');
+					listElement2.appendChild(space);
+					count2 = count2 - 1;
+				}
+				else
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Event Name: ';
+					listElement.appendChild(newRow)
+					listElement.appendChild(listItem);
+					space = document.createElement('br');
+					listElement.appendChild(space);
+					count2 = count2 + 1;
+				}
+				
+				listItem = document.createElement('li');
+				listItem.innerHTML = listData[i].date;
+				if(count3 == 1)
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Date: ';
+					listElement2.appendChild(newRow)
+					listElement2.appendChild(listItem);
+					space = document.createElement('br');
+					listElement2.appendChild(space);
+					count3 = count3 - 1;
+				}
+				else
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Date: ';
+					listElement.appendChild(newRow)
+					listElement.appendChild(listItem);
+					space = document.createElement('br');
+					listElement.appendChild(space);
+					count3 = count3 + 1;
+				}
+				
+				listItem = document.createElement('li');
+				listItem.innerHTML = listData[i].description;
+				if(count4 == 1)
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Description: ';
+					listElement2.appendChild(newRow)
+					listElement2.appendChild(listItem);
+					space = document.createElement('br');
+					listElement2.appendChild(space);
+					count4 = count4 - 1;
+				}
+				else
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Description: ';
+					listElement.appendChild(newRow)
+					listElement.appendChild(listItem);
+					space = document.createElement('br');
+					listElement.appendChild(space);
+					count4 = count4 + 1;
+				}
+				
+				listItem = document.createElement('li');
+				listItem.innerHTML = listData[i].type;
+				if(count5 == 1)
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Type of Event: ';
+					listElement2.appendChild(newRow)
+					listElement2.appendChild(listItem);
+					space = document.createElement('br');
+					listElement2.appendChild(space);
+					count5 = count5 - 1;
+				}
+				else
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Type of Event:';
+					listElement.appendChild(newRow)
+					listElement.appendChild(listItem);
+					space = document.createElement('br');
+					listElement.appendChild(space);
+					count5 = count5 + 1;
+				}
+				
+				listItem = document.createElement('li');
+				listItem.innerHTML = listData[i].color;
+				if(count6 == 1)
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Color:';
+					listElement2.appendChild(newRow)
+					listElement2.appendChild(listItem);
+					space = document.createElement('br');
+					listElement2.appendChild(space);
+					count6 = count6 - 1;
+				}
+				else
+				{
+					newRow = document.createElement('p');
+					newRow.innerText = 'Color:';
+					listElement.appendChild(newRow)
+					listElement.appendChild(listItem);
+					count6 = count6 + 1;
+				}
+				
+			}
+		};
+makeList();
+
+
+		$("#menu3").click(function()
+		{
+			$('#eventlist').css("z-index", 10);
+			$('#calendar').css("z-index", -10);
+			$('#eventform').css("z-index", -10);
+		});
 
 		$("#menu2").click(function()
 		{
 			$('#calendar').css("z-index", -10);
-			$('#eventform').css("z-index", 10);
+			$('#eventform').css("z-index", 20);
+			$('#eventlist').css("z-index", -20);
 		});
 
 		$("#menu1").click(function()
@@ -54,5 +229,6 @@ $(document).ready(function() {
 			$('#calendar').css("z-index", 10);
 			$('#bubble1').css("z-index", 10);
 			$('#eventform').css("z-index", -10);
+			$('#eventlist').css("z-index", -10);
 		});
 		
